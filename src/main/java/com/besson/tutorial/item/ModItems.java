b6e4d7c16c94ebcfe985213d4c0029407f01fe3f
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-12-06 00:57:58 +0800

    ARMOR EFFECT

--- src/main/java/com/besson/tutorial/item/ModItems.java
+++ src/main/java/com/besson/tutorial/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.besson.tutorial.item;
 
 import com.besson.tutorial.TutorialMod;
+import com.besson.tutorial.item.custom.CustomArmorItem;
 import com.besson.tutorial.item.custom.ModFuelItem;
 import com.besson.tutorial.item.custom.PickaxeAxeItem;
 import com.besson.tutorial.item.custom.ProspectorItem;
@@ -51,7 +52,7 @@ public class ModItems {
             () -> new PickaxeAxeItem(ModToolTiers.FIRE_ETHER, 6.0F, -2.8F, new Item.Properties().fireResistant()));
 
     public static final RegistryObject<Item> ICE_ETHER_HELMET = ITEMS.register("ice_ether_helmet",
-            () -> new ArmorItem(ModArmorMaterials.ICE_ETHER, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new CustomArmorItem(ModArmorMaterials.ICE_ETHER, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> ICE_ETHER_CHESTPLATE = ITEMS.register("ice_ether_chestplate",
             () -> new ArmorItem(ModArmorMaterials.ICE_ETHER, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> ICE_ETHER_LEGGINGS = ITEMS.register("ice_ether_leggings",
